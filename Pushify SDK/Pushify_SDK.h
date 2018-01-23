3d2b61f28df3396c39bec6933d291f23b8bad87c
Xavier Maroñas <orcrist@telefonica.net>
2018-01-23 11:53:44 +0100

    Initial Commit

---
+++ Pushify SDK/Pushify_SDK.h
@@ -0,0 +1,19 @@
+//
+//  Pushify_SDK.h
+//  Pushify SDK
+//
+//  Created by Xavier Maroñas on 23/1/18.
+//  Copyright © 2018 Liquid Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pushify_SDK.
+FOUNDATION_EXPORT double Pushify_SDKVersionNumber;
+
+//! Project version string for Pushify_SDK.
+FOUNDATION_EXPORT const unsigned char Pushify_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pushify_SDK/PublicHeader.h>
+
+
