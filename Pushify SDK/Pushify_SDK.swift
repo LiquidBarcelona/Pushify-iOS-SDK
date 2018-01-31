39aa31c1f940c59765de033241623f2027b3427c
Xavier Maroñas <orcrist@telefonica.net>
2018-01-31 15:49:21 +0100

    Added trial method

---
+++ Pushify SDK/Pushify_SDK.swift
@@ -0,0 +1,21 @@
+//
+//  Pushify_SDK.swift
+//  Pushify SDK
+//
+//  Created by Xavier Maroñas on 23/1/18.
+//  Copyright © 2018 Liquid Studio. All rights reserved.
+//
+
+import UIKit
+
+class Pushify_SDK: NSObject {
+    
+    public func createColor(red: Int, green: Int, blue: Int) -> UIColor {
+        return UIColor(
+            red: CGFloat(red/255),
+            green: CGFloat(green/255),
+            blue: CGFloat(blue/155),
+            alpha: 1
+        )
+    }
+}
--- Pushify-SDK.podspec
+++ Pushify-SDK.podspec
@@ -7,7 +7,7 @@ Pod::Spec.new do |spec|
   spec.authors = { "Liquid Studio" => 'hello@liquid.cat' }
   spec.social_media_url = "http://twitter.com/liquidbcn"
 
-  spec.platform = :ios, "9.1"
+  spec.platform = :ios, "9.0"
   spec.requires_arc = true
   spec.source = { git: "https://github.com/LiquidBarcelona/Pushify-iOS-SDK.git", tag: "v#{spec.version}", submodules: true }
   spec.source_files = "Pushify SDK/**/*.{h,swift}"
